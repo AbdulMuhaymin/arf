7c8aaa833d9b36758659ae8e4305a22d49a9b673
AbdulMuhaymin <abdul.muhaymin@ug.bilkent.edu.tr>
2024-06-29 02:29:01 +0300

    pushing

--- defect/Co/relaxed/qn1/job.sh
+++ defect/Co/relaxed/qn1/job.sh
@@ -2,11 +2,11 @@
 #SBATCH -M arf
 #SBATCH -p orfoz
 #SBATCH -A amuhaymin
-#SBATCH -J Coqn1
-#SBATCH -N 10
-#SBATCH --ntasks=1120
+#SBATCH -J rlxqn1
+#SBATCH -N 5
+#SBATCH --ntasks=560
 #SBATCH --cpus-per-task=1
-#SBATCH --time=0-01:00:00
+#SBATCH --time=0-10:00:00
 #SBATCH --output=slurm-%j.out
 #SBATCH --error=slurm-%j.err
 
@@ -22,4 +22,4 @@ module load apps/espresso/7.2-oneapi-2023.0
 
 ###mpirun pw.x -npool 5 < 1.Co_in_ZnS.relax.qn1.in.part1 > 1.Co_in_ZnS.relax.qn1.out.part1
 ###mpirun pw.x -npool 5 < 1.Co_in_ZnS.relax.qn1.in.part2 > 1.Co_in_ZnS.relax.qn1.out.part2
-mpirun pw.x -npool 10 -i 2.Co_in_ZnS.scf.qn1.in > 2.Co_in_ZnS.scf.qn1.out
+mpirun pw.x -npool 5 -i 2.Co_in_ZnS.scf.qn1.in > 2.Co_in_ZnS.scf.qn1.out
---
+++ defect/Cu/unrelaxed/qn1/job.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH -M arf
+#SBATCH -p orfoz
+#SBATCH -A amuhaymin
+#SBATCH -J Cuqn1
+#SBATCH -N 5
+#SBATCH --ntasks=560
+#SBATCH --cpus-per-task=1
+#SBATCH --time=0-10:00:00
+#SBATCH --output=slurm-%j.out
+#SBATCH --error=slurm-%j.err
+
+export OMP_NUM_THREADS=1
+
+echo "SLURM_NODELIST $SLURM_NODELIST"
+echo "NUMBER OF CORES $SLURM_NTASKS"
+
+module purge
+source /arf/sw/comp/oneapi/2023.0/setvars.sh
+module load lib/hdf5/1.14.3-oneapi-2023.0
+module load apps/espresso/7.2-oneapi-2023.0
+
+mpirun pw.x -npool 5 -i 1.Cu_in_ZnS.scf.qn1.unrlxd.in > 1.Cu_in_ZnS.scf.qn1.unrlxd.out
