8a85be56fab21b5270956719117fca4913c2a3da
AbdulMuhaymin <abdul.muhaymin@ug.bilkent.edu.tr>
2024-07-05 02:43:03 +0300

    pushing

--- defect/Cu-vacancy/relaxed/qn1/job.sh
+++ defect/Cu-vacancy/relaxed/qn1/job.sh
@@ -2,9 +2,9 @@
 #SBATCH -M arf
 #SBATCH -p orfoz
 #SBATCH -A amuhaymin
-#SBATCH -J CuVqn1rlx
-#SBATCH -N 5
-#SBATCH --ntasks=560
+#SBATCH -J CuVqn1
+#SBATCH -N 13
+#SBATCH --ntasks=1456
 #SBATCH --cpus-per-task=1
 #SBATCH --time=0-10:00:00
 #SBATCH --output=slurm-%j.out
@@ -24,14 +24,15 @@ module load apps/espresso/7.2-oneapi-2023.0
 mpirun pw.x -npool 13 -i 2.Cu_vac_in_ZnS.scf.qn1.in > 2.Cu_vac_in_ZnS.scf.qn1.out
 
 
-mpirun pw.x -npool 13 -i 3.Cu_vac_in_ZnS.nscf.q0.in > 3.Cu_vac_in_ZnS.nscf.q0.out
+mpirun pw.x -npool 13 -i 3.Cu_vac_in_ZnS.nscf.qn1.in > 3.Cu_vac_in_ZnS.nscf.qn1.out
 
 mpirun -np 112 pp.x < 4.pp.in > 4.pp.out
 mpirun -np 112 dos.x < 5.dos.in > 5.dos.out
 mpirun -np 112 projwfc.x < 6.projwfc.in > 6.projwfc.out
 
-tar -czvf 6.pdos_files.tar.gz Cu_vac_in_ZnS.q0.pdos*
+tar -czvf 6.pdos_files.tar.gz Cu_vac_in_ZnS.qn1.pdos*
 tar -tzvf 6.pdos_files.tar.gz
-rm Cu_vac_in_ZnS.q0.pdos*
+rm Cu_vac_in_ZnS.qn1.pdos*
+rm Cu_vac_in_ZnS.qn1.pp
 rm -rf out/*/*wfc*.hdf5
 
--- defect/Cu-vacancy/relaxed/qp1/job.sh
+++ defect/Cu-vacancy/relaxed/qp1/job.sh
@@ -2,9 +2,9 @@
 #SBATCH -M arf
 #SBATCH -p orfoz
 #SBATCH -A amuhaymin
-#SBATCH -J CuVqp1rlx
-#SBATCH -N 5
-#SBATCH --ntasks=560
+#SBATCH -J CuVqp1
+#SBATCH -N 13
+#SBATCH --ntasks=1456
 #SBATCH --cpus-per-task=1
 #SBATCH --time=0-10:00:00
 #SBATCH --output=slurm-%j.out
@@ -22,4 +22,17 @@ module load apps/espresso/7.2-oneapi-2023.0
 
 ###mpirun pw.x -npool 5 -i 1.Cu_vac_in_ZnS.relax.qp1.in.part1 > 1.Cu_vac_in_ZnS.relax.qp1.out.part1
 ###mpirun pw.x -npool 5 -i 1.Cu_vac_in_ZnS.relax.qp1.in.part2 > 1.Cu_vac_in_ZnS.relax.qp1.out.part2
-mpirun pw.x -npool 5 -i 2.Cu_vac_in_ZnS.scf.qp1.in > 2.Cu_vac_in_ZnS.scf.qp1.out
+mpirun pw.x -npool 13 -i 2.Cu_vac_in_ZnS.scf.qp1.in > 2.Cu_vac_in_ZnS.scf.qp1.out
+
+
+mpirun pw.x -npool 13 -i 3.Cu_vac_in_ZnS.nscf.qp1.in > 3.Cu_vac_in_ZnS.nscf.qp1.out
+
+mpirun -np 112 pp.x < 4.pp.in > 4.pp.out
+mpirun -np 112 dos.x < 5.dos.in > 5.dos.out
+mpirun -np 112 projwfc.x < 6.projwfc.in > 6.projwfc.out
+
+tar -czvf 6.pdos_files.tar.gz Cu_vac_in_ZnS.qp1.pdos*
+tar -tzvf 6.pdos_files.tar.gz
+rm Cu_vac_in_ZnS.qp1.pdos*
+rm Cu_vac_in_ZnS.qp1.pp
+rm -rf out/*/*wfc*.hdf5
