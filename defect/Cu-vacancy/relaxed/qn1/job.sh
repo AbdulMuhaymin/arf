1ae84dc6c81f15282639986f07101c2216a5ae71
AbdulMuhaymin <abdul.muhaymin@ug.bilkent.edu.tr>
2024-07-04 23:47:15 +0300

    pushing

--- defect/Cu-vacancy/relaxed/qn1/job.sh
+++ defect/Cu-vacancy/relaxed/qn1/job.sh
@@ -21,4 +21,17 @@ module load lib/hdf5/1.14.3-oneapi-2023.0
 module load apps/espresso/7.2-oneapi-2023.0
 
 ###mpirun pw.x -npool 5 -i 1.Cu_vac_in_ZnS.relax.qn1.in > 1.Cu_vac_in_ZnS.relax.qn1.out
-mpirun pw.x -npool 5 -i 2.Cu_vac_in_ZnS.scf.qn1.in > 2.Cu_vac_in_ZnS.scf.qn1.out
+mpirun pw.x -npool 13 -i 2.Cu_vac_in_ZnS.scf.qn1.in > 2.Cu_vac_in_ZnS.scf.qn1.out
+
+
+mpirun pw.x -npool 13 -i 3.Cu_vac_in_ZnS.nscf.q0.in > 3.Cu_vac_in_ZnS.nscf.q0.out
+
+mpirun -np 112 pp.x < 4.pp.in > 4.pp.out
+mpirun -np 112 dos.x < 5.dos.in > 5.dos.out
+mpirun -np 112 projwfc.x < 6.projwfc.in > 6.projwfc.out
+
+tar -czvf 6.pdos_files.tar.gz Cu_vac_in_ZnS.q0.pdos*
+tar -tzvf 6.pdos_files.tar.gz
+rm Cu_vac_in_ZnS.q0.pdos*
+rm -rf out/*/*wfc*.hdf5
+
