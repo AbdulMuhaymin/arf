4ba089c06693ff0087d5a19a3064680dd66d83f2
AbdulMuhaymin <abdul.muhaymin@ug.bilkent.edu.tr>
2024-07-05 11:30:11 +0300

    pushing

--- defect/Ni/relaxed/qp1/job.sh
+++ defect/Ni/relaxed/qp1/job.sh
@@ -21,11 +21,11 @@ module load lib/hdf5/1.14.3-oneapi-2023.0
 module load apps/espresso/7.2-oneapi-2023.0
 
 ###mpirun pw.x -npool 5 -i 1.Ni_in_ZnS.relax.qp1.in > 1.Ni_in_ZnS.relax.qp1.out
-mpirun pw.x -npool 10 -i 2.Ni_in_ZnS.scf.qp1.in > 2.Ni_in_ZnS.scf.qp1.out
+###mpirun pw.x -npool 10 -i 2.Ni_in_ZnS.scf.qp1.in > 2.Ni_in_ZnS.scf.qp1.out
 
 mpirun pw.x -npool 10 -i 3.Ni_in_ZnS.nscf.qp1.in > 3.Ni_in_ZnS.nscf.qp1.out
 
-###mpirun -np 112 pp.x < 4.pp.in > 4.pp.out
+mpirun -np 112 pp.x < 4.pp.in > 4.pp.out
 mpirun -np 112 dos.x < 5.dos.in > 5.dos.out
 mpirun -np 112 projwfc.x < 6.projwfc.in > 6.projwfc.out
 
