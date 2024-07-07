811ebcc38dad790bd4313e653a02bb9937c96029
AbdulMuhaymin <abdul.muhaymin@ug.bilkent.edu.tr>
2024-07-07 03:10:03 +0300

    pushing

--- defect/Co-vacancy/relaxed/q0/job.sh
+++ defect/Co-vacancy/relaxed/q0/job.sh
@@ -2,9 +2,9 @@
 #SBATCH -M arf
 #SBATCH -p orfoz
 #SBATCH -A amuhaymin
-#SBATCH -J CoVrlxq0
-#SBATCH -N 5
-#SBATCH --ntasks=560
+#SBATCH -J CoVq0r
+#SBATCH -N 7
+#SBATCH --ntasks=784
 #SBATCH --cpus-per-task=1
 #SBATCH --time=0-10:00:00
 #SBATCH --output=slurm-%j.out
@@ -20,4 +20,17 @@ source /arf/sw/comp/oneapi/2023.0/setvars.sh
 module load lib/hdf5/1.14.3-oneapi-2023.0
 module load apps/espresso/7.2-oneapi-2023.0
 
-mpirun pw.x -npool 5 -i 1.Co_vac_in_ZnS.relax.q0.in.part2 > 1.Co_vac_in_ZnS.relax.q0.out.part2
+###mpirun pw.x -npool 5 -i 1.Co_vac_in_ZnS.relax.q0.in.part2 > 1.Co_vac_in_ZnS.relax.q0.out.part2
+
+mpirun pw.x -npool 7 -i 2.Co_vac_in_ZnS.scf.q0.in > 2.Co_vac_in_ZnS.scf.q0.out
+
+mpirun pw.x -npool 7 -i 3.Co_vac_in_ZnS.nscf.q0.in > 3.Co_vac_in_ZnS.nscf.q0.out
+
+mpirun -np 112 pp.x < 4.pp.in > 4.pp.out
+mpirun -np 112 dos.x < 5.dos.in > 5.dos.out
+mpirun -np 112 projwfc.x < 6.projwfc.in > 6.projwfc.out
+
+tar -czvf 6.pdos_files.tar.gz Co_vac_in_ZnS.q0.pdos*
+tar -tzvf 6.pdos_files.tar.gz
+rm Co_vac_in_ZnS.q0.pdos*
+rm -rf out/*/*wfc*.hdf5
