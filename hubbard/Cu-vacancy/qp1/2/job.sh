aef0cb907a297da6538d8a3f155db1b03bef1dd5
AbdulMuhaymin <abdul.muhaymin@ug.bilkent.edu.tr>
2024-07-10 22:56:08 +0300

    pushing

---
+++ hubbard/Cu-vacancy/qp1/2/job.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH -M arf
+#SBATCH -p orfoz
+#SBATCH -A amuhaymin
+#SBATCH -J qp1HU2
+#SBATCH -N 7
+#SBATCH --ntasks=784
+#SBATCH --cpus-per-task=1
+#SBATCH --time=2-23:00:00
+#SBATCH --output=slurm-%j.out
+#SBATCH --error=slurm-%j.err
+
+export OMP_NUM_THREADS=1
+
+echo "SLURM_NODELIST $SLURM_NODELIST"
+echo "NUMBER OF CORES $SLURM_NTASKS"
+
+module purge
+source /arf/sw/comp/oneapi/2023.0/setvars.sh
+module load lib/hdf5/1.14.3-oneapi-2023.0
+module load apps/espresso/7.2-oneapi-2023.0
+
+mpirun pw.x -npool 7 -i 1.Cu_vac_in_ZnS.relax.qp1.U.2.in > 1.Cu_vac_in_ZnS.relax.qp1.U.2.out
