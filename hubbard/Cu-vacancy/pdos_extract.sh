ba58f9990531fc150006a8e95770b1df54d11459
AbdulMuhaymin <abdul.muhaymin@ug.bilkent.edu.tr>
2024-07-12 01:00:53 +0300

    pushing

---
+++ hubbard/Cu-vacancy/pdos_extract.sh
@@ -0,0 +1,7 @@
+mkdir 6.pdos_files
+tar -zxvf 6.pdos_files.tar.gz -C 6.pdos_files/
+cd 6.pdos_files/
+sumpdos.x *\(Zn\)* > Zn.pdos
+sumpdos.x *\(S\)* > S.pdos
+sumpdos.x *\(Cu\)* > Cu.pdos
+sumpdos.x *\(Cu\)*3* > Cu_3d.pdos
