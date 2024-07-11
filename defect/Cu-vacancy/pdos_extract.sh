cabb97e2976262fba89a84ff972a3ab061172959
AbdulMuhaymin <abdul.muhaymin@ug.bilkent.edu.tr>
2024-07-11 15:52:56 +0300

    pushing

--- defect/Cu-vacancy/pdos_extract.sh
+++ defect/Cu-vacancy/pdos_extract.sh
@@ -3,5 +3,5 @@ tar -zxvf 6.pdos_files.tar.gz -C 6.pdos_files/
 cd 6.pdos_files/
 sumpdos.x *\(Zn\)* > Zn.pdos
 sumpdos.x *\(S\)* > S.pdos
-sumpdos.x *\(Co\)* > Co.pdos
-sumpdos.x *\(Co\)*3* > Co_3d.pdos
+sumpdos.x *\(Cu\)* > Cu.pdos
+sumpdos.x *\(Cu\)*3* > Cu_3d.pdos
