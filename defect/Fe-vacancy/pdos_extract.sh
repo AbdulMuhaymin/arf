2376c3cca7b552f6b75f4acd1527461614b9467b
AbdulMuhaymin <abdul.muhaymin@ug.bilkent.edu.tr>
2024-07-12 00:40:09 +0300

    pushing

--- defect/Fe-vacancy/pdos_extract.sh
+++ defect/Fe-vacancy/pdos_extract.sh
@@ -3,5 +3,5 @@ tar -zxvf 6.pdos_files.tar.gz -C 6.pdos_files/
 cd 6.pdos_files/
 sumpdos.x *\(Zn\)* > Zn.pdos
 sumpdos.x *\(S\)* > S.pdos
-sumpdos.x *\(Fe\)* > Co.pdos
-sumpdos.x *\(Fe\)*3* > Co_3d.pdos
+sumpdos.x *\(Fe\)* > Fe.pdos
+sumpdos.x *\(Fe\)*3* > Fe_3d.pdos
