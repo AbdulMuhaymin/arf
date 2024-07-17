4f020762138063cc47f16b8098fd4bd47a2ff261
AbdulMuhaymin <abdul.muhaymin@ug.bilkent.edu.tr>
2024-07-18 00:57:58 +0300

    pushing

--- hubbard/Cu-vacancy/relaxed/q0/3/job.sh
+++ hubbard/Cu-vacancy/relaxed/q0/3/job.sh
@@ -2,9 +2,9 @@
 #SBATCH -M arf
 #SBATCH -p orfoz
 #SBATCH -A amuhaymin
-#SBATCH -J q0HU3
-#SBATCH -N 7
-#SBATCH --ntasks=784
+#SBATCH -J q0U3r
+#SBATCH -N 6
+#SBATCH --ntasks=672
 #SBATCH --cpus-per-task=1
 #SBATCH --time=2-23:00:00
 #SBATCH --output=slurm-%j.out
@@ -20,4 +20,18 @@ source /arf/sw/comp/oneapi/2023.0/setvars.sh
 module load lib/hdf5/1.14.3-oneapi-2023.0
 module load apps/espresso/7.2-oneapi-2023.0
 
-mpirun pw.x -npool 7 -i 1.Cu_vac_in_ZnS.relax.q0.U.3.in > 1.Cu_vac_in_ZnS.relax.q0.U.3.out
+###mpirun pw.x -npool 7 -i 1.Cu_vac_in_ZnS.relax.q0.U.3.in > 1.Cu_vac_in_ZnS.relax.q0.U.3.out
+mpirun pw.x -npool 6 -i 2.Cu_vac_in_ZnS.scf.q0.U3.in > 2.Cu_vac_in_ZnS.scf.q0.U3.out
+mpirun pw.x -npool 6 -i 3.Cu_vac_in_ZnS.nscf.q0.U3.in > 3.Cu_vac_in_ZnS.nscf.q0.U3.out
+
+mpirun -np 112 pp.x < 4.pp.in > 4.pp.out
+mpirun -np 112 dos.x < 5.dos.in > 5.dos.out
+mpirun -np 112 projwfc.x < 6.projwfc.in > 6.projwfc.out
+
+rm out/*.hub*
+rm out/*/wfc*.hdf5
+tar -czvf 6.pdos_files.tar.gz Cu_vac_in_ZnS.q0.U3.pdos*
+tar -tzvf 6.pdos_files.tar.gz
+rm Cu_vac_in_ZnS.q0.U3.pdos*
+rm Cu_vac_in_ZnS.q0.U3.pp
+
--- hubbard/Cu-vacancy/unrelaxed/q0/2/job.sh
+++ hubbard/Cu-vacancy/unrelaxed/q0/2/job.sh
@@ -2,9 +2,9 @@
 #SBATCH -M arf
 #SBATCH -p orfoz
 #SBATCH -A amuhaymin
-#SBATCH -J q0U2r
-#SBATCH -N 13
-#SBATCH --ntasks=1456
+#SBATCH -J CuVppU2
+#SBATCH -N 1
+#SBATCH --ntasks=112
 #SBATCH --cpus-per-task=1
 #SBATCH --time=2-23:00:00
 #SBATCH --output=slurm-%j.out
@@ -22,15 +22,14 @@ module load apps/espresso/7.2-oneapi-2023.0
 
 #####mpirun pw.x -npool 7 -i 1.Cu_vac_in_ZnS.relax.q0.U.2.in.part1 > 1.Cu_vac_in_ZnS.relax.q0.U.2.out.part3
 
-mpirun pw.x -npool 13 -i 2.Cu_vac_in_ZnS.scf.q0.U2.in > 2.Cu_vac_in_ZnS.scf.q0.U2.out
-mpirun pw.x -npool 13 -i 3.Cu_vac_in_ZnS.nscf.q0.U2.in > 3.Cu_vac_in_ZnS.nscf.q0.U2.out
+####mpirun pw.x -npool 7 -i 2.Cu_vac_in_ZnS.scf.q0.U.2.in > 2.Cu_vac_in_ZnS.scf.q0.U.2.out
+####mpirun pw.x -npool 7 -i 3.Cu_vac_in_ZnS.nscf.q0.U.2.in > 3.Cu_vac_in_ZnS.nscf.q0.U.2.out
 
 mpirun -np 112 pp.x < 4.pp.in > 4.pp.out
 mpirun -np 112 dos.x < 5.dos.in > 5.dos.out
 mpirun -np 112 projwfc.x < 6.projwfc.in > 6.projwfc.out
-
-tar -czvf 6.pdos_files.tar.gz Cu_vac_in_ZnS.q0.U2.pdos*
+tar -czvf 6.pdos_files.tar.gz Cu_vac_in_ZnS.q0.U.2.pdos*
 tar -tzvf 6.pdos_files.tar.gz
-rm Cu_vac_in_ZnS.q0.U2.pdos*
-rm Cu_vac_in_ZnS.q0.U2.pp
+rm Cu_vac_in_ZnS.q0.U.2.pdos*
+rm Cu_vac_in_ZnS.q0.U.2.pp
 
