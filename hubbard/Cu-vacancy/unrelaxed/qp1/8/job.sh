0de68efb92d6d32dd1c62fabe91d6c2ec166a356
AbdulMuhaymin <abdul.muhaymin@ug.bilkent.edu.tr>
2024-07-25 07:58:48 +0300

    pushing

---
+++ hubbard/Cu-vacancy/unrelaxed/qp1/8/job.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#SBATCH -M arf
+#SBATCH -p orfoz
+#SBATCH -A amuhaymin
+#SBATCH -J U8unr
+#SBATCH -N 13
+#SBATCH --ntasks=1456
+#SBATCH --cpus-per-task=1
+#SBATCH --time=2-23:00:00
+#SBATCH --output=slurm-%j.out
+#SBATCH --error=slurm-%j.err
+
+export OMP_NUM_THREADS=1
+
+echo "SLURM_NODELIST $SLURM_NODELIST"
+echo "NUMBER OF CORES $SLURM_NTASKS"
+
+module purge
+source /arf/sw/comp/oneapi/2023.0/setvars.sh
+module load lib/hdf5/1.14.3-oneapi-2023.0
+module load apps/espresso/7.2-oneapi-2023.0
+
+mpirun pw.x -npool 13 -i 1.Cu_vac_in_ZnS.scf.qp1.unrlxd.U8.in > 1.Cu_vac_in_ZnS.scf.qp1.unrlxd.U8.out
+mpirun pw.x -npool 13 -i 2.Cu_vac_in_ZnS.nscf.qp1.unrlxd.U8.in > 2.Cu_vac_in_ZnS.nscf.qp1.unrlxd.U8.out
+
+mpirun -np 112 pp.x < 4.pp.in > 4.pp.out
+mpirun -np 112 dos.x < 5.dos.in > 5.dos.out
+mpirun -np 112 projwfc.x < 6.projwfc.in > 6.projwfc.out
+
+rm out/*.hub*
+###rm out/*/wfc*.hdf5
+tar -czvf 6.pdos_files.tar.gz Cu_vac_in_ZnS.qp1.unrlxd.U8.pdos*
+tar -tzvf 6.pdos_files.tar.gz
+rm Cu_vac_in_ZnS.qp1.unrlxd.U8.pdos*
+rm Cu_vac_in_ZnS.qp1.unrlxd.U8.pp
+
