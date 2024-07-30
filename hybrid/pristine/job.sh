43b71c0a1f3b035e0c2ab83634ec691de906bb09
AbdulMuhaymin <abdul.muhaymin@ug.bilkent.edu.tr>
2024-07-30 04:56:09 +0300

    pushing

---
+++ hybrid/pristine/job.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH -M arf
+#SBATCH -p orfoz
+#SBATCH -A amuhaymin
+#SBATCH -J hybZnS
+#SBATCH -N 3
+#SBATCH --ntasks=336
+#SBATCH --cpus-per-task=1
+#SBATCH --time=0-15:00:00
+#SBATCH --output=slurm-%j.out
+#SBATCH --error=slurm-%j.err
+
+export OMP_NUM_THREADS=1
+
+echo "SLURM_NODELIST $SLURM_NODELIST"
+echo "NUMBER OF CORES $SLURM_NTASKS"
+
+module purge
+source /arf/sw/comp/oneapi/2023.0/setvars.sh
+module load lib/hdf5/1.14.3-oneapi-2023.0
+module load apps/espresso/7.2-oneapi-2023.0
+
+mpirun -np 336 pw.x -nb 3 -i 1.ZnS.scf.hse06.in > 1.ZnS.scf.hse06.out
